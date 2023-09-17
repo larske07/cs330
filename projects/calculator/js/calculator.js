29959950b84ac19fae12d48c8817a7ac79fec7c0
Keagan Larson <larske07@luther.edu>
2023-09-17 15:56:35 -0500

    Finish project calculator

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -11,19 +11,58 @@ var clearOnEntry;
  * 
  * @param {number} digit digit to add or display on the `outputScreen`
  */
-function enterDigit(digit) {}
+function enterDigit(digit) {
+    outputScreen.textContent += digit;
+}
 
 
 /**
  * Clear `outputScreen` and set value to 0
  */
-function clear_screen() {}
+function clear_screen() {
+    outputScreen.innerHTML = "";
+    outputScreen.textContent = "0";
+}
 
 
 /**
  * Evaluate the expression and display its result or *ERROR*
  */
-function eval_expr() {}
+function eval_expr() {
+    // split up the string depending on what is present
+    var resultElement = document.getElementById("result");
+    var expression = resultElement.innerHTML;
+    
+
+    if (expression.includes("&nbsp;")) {
+        var splitArray = expression.split("&nbsp;");
+        expression = splitArray[1];
+    }
+
+    if (expression === "") {
+        expression = "0";
+      }
+
+    if (resultElement.innerHTML === "undefined") {
+        expression = "0";
+    }
+
+
+    // Try to evaluate the expression
+    let result;
+    try {
+
+        // Print the expression to the console for testing purposes
+        console.log(`Evaluating expression: ${expression}`);
+
+        result = eval(expression);
+    } catch (error) {
+        result = "ERROR";
+    }
+
+    // Display the result
+    document.getElementById("result").innerHTML = result;
+}
 
 
 /**
@@ -31,7 +70,9 @@ function eval_expr() {}
  * 
  * @param {string} operation to add to the expression
  */
-function enterOp(operation) {}
+function enterOp(operation) {
+    outputScreen.textContent += operation;
+}
 
 
 window.onload = function () {
