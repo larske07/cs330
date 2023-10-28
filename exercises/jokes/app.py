015467493595853af29ad3e023a123db2cb3e1a3
Keagan Larson <larske07@luther.edu>
2023-10-28 17:07:48 -0500

    finished jokes

--- exercises/jokes/app.py
+++ exercises/jokes/app.py
@@ -5,25 +5,43 @@ import random
 from typing import List
 
 import pyjokes
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    """Render the template with form"""
-    raise NotImplementedError
+    return render_template("base.html")
 
 
 @app.route("/", methods=["POST"])
 def index_jokes():
     """Render the template with jokes"""
-    raise NotImplementedError
+    language = request.form.get("language")
+    category = request.form.get("category")
+    number = int(request.form.get("number"))
+
+    try:
+        jokes = send_joke(language, category, number)
+    except ValueError:
+        abort(404)
+    
+    return render_template("jokes.html", jokes=jokes, language=language, category=category, number=number)
 
 
 def send_joke(
     language: str = "en", category: str = "all", number: int = 1
 ) -> List[str]:
-    """Return a list of jokes"""
-    raise NotImplementedError
+    try:
+        if number == 1:
+            joke = pyjokes.get_joke(language=language, category=category)
+            return [joke]
+        else:
+            jokes = pyjokes.get_jokes(language=language, category=category)
+            if not jokes:
+                raise ValueError
+        return jokes
+    except Exception as e:
+        print(e)
+        return [-1]
