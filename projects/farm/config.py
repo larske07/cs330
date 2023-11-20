62fa299645b9ac3989c5ae2b645f740e04d7ed9b
Keagan Larson <larske07@luther.edu>
2023-11-19 20:32:35 -0600

    updated

--- projects/farm/config.py
+++ projects/farm/config.py
@@ -3,6 +3,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///farmtotable.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///farmtotable.sqlite'
 
 db = SQLAlchemy(app)
--- projects/farm/models.py
+++ projects/farm/models.py
@@ -1,6 +1,7 @@
 from config import db
 
 class Product(db.Model):
+    __tablename__ = 'product'
     id = db.Column(db.Integer, primary_key=True) 
     name = db.Column(db.String(100), nullable=False) 
     description = db.Column(db.Text) 
@@ -9,6 +10,7 @@ class Product(db.Model):
     image = db.Column(db.String(150)) 
   
 class CartItem(db.Model):
+    __tablename__ = 'cartitem'
     id = db.Column(db.Integer, primary_key=True) 
     product_id = db.Column(db.Integer, db.ForeignKey('product.id')) 
     quantity = db.Column(db.Integer, default=1) 
